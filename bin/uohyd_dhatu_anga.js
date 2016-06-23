75d2183a25b1777dad49ae07be973690c8d17b2c
Michael Bykov <m.bykov@gmail.com>
2016-06-23 22:41:05 +0300

    i

--- bin/uohyd_dhatu_anga.js
+++ bin/uohyd_dhatu_anga.js
@@ -27,9 +27,12 @@ var nmap = {'0': 'sg', '1': 'du', '2': 'pl'};
 // вычитаю -tins, собираю stems - д.б. ровно один stem для gana=1, la=law
 // нужно прогнать все глаголы для всех gana-lakara
 
+var stems = {};
+
 verbs.forEach(function(verb) {
     if (verb.gana != 1) return;
     // p(verb.dhatu);
+    stems[verb.dhatu] = {strong: [], weak: []}; // <<===
     for (var pada in verb.la) {
         log(pada);
         var lakaras = verb.la[pada];
@@ -37,21 +40,60 @@ verbs.forEach(function(verb) {
             if (la != 'लट्') continue;
             var numbers = lakaras[la];
             log(la);
+            var strongs = [];
+            var weaks = [];
             for (var number in numbers) {
+                // log('N', number);
                 var forms = numbers[number];
                 forms.forEach(function(form, idx) {
                     var purusha = nmap[idx.toString()];
-                    var numper = [number, purusha].join('.');
-                    log(numper, form);
+                    log('P', purusha);
+                    var numper = [purusha, number].join('.');
+                    // log(numper, form);
+                    // log(la, pada);
+                    var stins = selectTins(la, pada, numper); // можно заранне tin_for_numper
+                    // log('CTINS', stins.length, stins);
+                    var errTooMore = ['too more stins:', verb.dhatu, pada, la, number, numper, stins].join(' - ');
+                    var stin;
+                    if (stins.length > 1) throw new Error(errTooMore); // тут чудовищно:
+                    else stin = stins[0];
+                    // log('STIN', stin);
+                    // log('TERM', Object.keys(stin));
+                    var term = Object.keys(stin)[0];
+                    var errNoStem = ['no stem:', verb.dhatu, pada, la, number, numper, term].join(' - ');
+                    var re = new RegExp(term + '$');
+                    var stem = form.replace(re, '');
+                    // log('F=S', form, stem, stin);
+                    if (form == stem) throw new Error(errNoStem);
+                    if (number == '1') strongs.push(stem);
+                    else weaks.push(stem);
+                    // var result = {};
+                    // result[numper] = [stem, term];
+                    // stems[verb.dhatu].push(result);
                 });
             }
+            var errStrong = ['strong:', verb.dhatu, pada, la, number].join(' - ');
+            var errWeak = ['strong:', verb.dhatu, pada, la, number].join(' - ');
+            var ustrong = _.uniq(strongs);
+            var uweak = _.uniq(weaks);
+            if (ustrong.length > 1) throw new Error(errStrong);
+            if (uweak.length > 1) throw new Error(errWeak);
+            stems[verb.dhatu] = {strong: ustrong[0], weak: uweak[0]};
         }
     }
 });
 
 // log(tins);
-function selectTins(la, pada) {
-    //
+log('===========');
+p(stems);
+
+function selectTins(la, pada, numper) {
+    var key, val;
+    return _.select(tins, function(tin) {
+        key = Object.keys(tin)[0];
+        val = tin[key];
+        return val.la == la && val.pada == pada && val.np == numper;
+    });
 }
 
 
@@ -60,6 +102,9 @@ function selectTins(la, pada) {
   в результате должно быть :
   dhatu: [{gana: 1, stem: 'xxx'}, {gana: 2, strong: 'xxx', weak: 'yyy'}]
   и так для всех dhatu
+  чудовищно: нужно вычесть все tins, образовать stem
+  stems - сгруппировать по numper,
+  в группе выбрать min, остальные отбросить
 */
 
 // log(verbs);
